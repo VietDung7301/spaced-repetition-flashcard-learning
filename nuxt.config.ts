3cf949918a683b4c19c72682d4489d3384d9092a
Dodo-aiday <donguyen33m@gmail.com>
2024-12-12 01:30:23 +0000

    Implement add card function

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+
   modules: ['@nuxt/ui'],
+
   nitro: {
     experimental: {
       database: true
@@ -16,5 +19,7 @@ export default defineNuxtConfig({
         url: 'postgresql://username:password@hostname:port/database_name'
       }
     }
-  }
+  },
+
+  compatibilityDate: '2024-12-11'
 });
---
+++ pages/add-word.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="header">
+        <h3>Add new </h3>
+    </div>
+
+    <form >
+        <label></label>
+        <label></label>
+        <label></label>
+    </form>
+</template>
