a39f2c72ff4560159eb2df3d2c9bed0ca01fcdc8
Hoàng Việt Dũng <vietdung.7301@gmail.com>
2024-12-16 08:27:55 +0000

    Update review screen

--- pages/review.vue
+++ pages/review.vue
@@ -1,23 +1,58 @@
 <script setup lang="ts">
-import type { CardQuestion } from '~/types/type';
+import { _backgroundColor } from '#tailwind-config/theme';
+
+import type { CardQuestion, QuestionOption } from '~/types/type';
 
 const { user_id } = storeToRefs(useAuthStore());
 
-const cardList = await $fetch<CardQuestion[]>(`/api/card/due?user_id=${user_id.value}`, {
+const cardList = ref()
+const currentOptionList = ref<QuestionOption[]>()
+const isShowFullWord = ref(false)
+
+cardList.value = await $fetch<CardQuestion[]>(`/api/card/due?user_id=${user_id.value}`, {
     method: "GET",
 })
-
-for (let card of cardList) {
-    card.questions = await $fetch(`/api/card/question_option/${card.word}`), {
+let currentCardIndex = ref(0)
+for (let card of cardList.value) {
+    $fetch<QuestionOption>(`/api/card/question_option/${card.word}`, {
         method: 'GET'
-    }
+    }).then((value: QuestionOption) => {
+        if (value !== null) {
+            card.options = value
+            currentOptionList.value = cardList.value[0].options
+        }
+        for (let option of card.options) {
+            option.bg_color = "white"
+        }
+    })
 }
 
-let currentCardIndex = 0
+const handleAnswer = (option: QuestionOption) => {
+    $fetch(`/api/card/${cardList.value[currentCardIndex.value].id}`, {
+        method: 'PUT',
+        body: {
+            isCorrect: option.isCorrect,
+            interval: cardList.value[currentCardIndex.value].interval,
+            ease_factor: cardList.value[currentCardIndex.value].ease_factor,
+            repetitions: cardList.value[currentCardIndex.value].repetitions
+        }
+    })
+    if (option.isCorrect) {
+        option.bg_color = "green"
+    } else {
+        option.bg_color = "red"
+        isShowFullWord.value = true
+    }
+    setTimeout(() => {
+        currentCardIndex.value++
+        currentOptionList.value = cardList.value[currentCardIndex.value].options
+    }, 1000)
+    
+}
 </script>
 
 <template>
-<div class="w-full flex justify-center pt-10">
+<div v-if="cardList.length" class="w-full flex justify-center pt-10">
     <div class="flex flex-col w-3/4">
         <div class="grid grid-cols-12 gap-2">
             <UBadge class="col-span-1 self-center">{{ currentCardIndex }}</UBadge>
@@ -29,11 +64,42 @@ let currentCardIndex = 0
         <div class="flex flex-col">
             <div>{{ cardList[currentCardIndex].meaning }}</div>
             <div class="grid grid-cols-2 gap-4">
-                <UButton v-for="option in cardList[currentCardIndex].questions" class="h-36 flex items-center justify-center">
+                <UButton v-for="(option, idx) in currentOptionList" 
+                    class="h-36 flex items-center justify-center"
+                    :key="idx"
+                    :style="{'background-color': option.bg_color}"
+                    @click="handleAnswer(option)">
                     {{ option.word }}
                 </UButton>
             </div>
         </div>
     </div>
 </div>
+<div v-else>
+    <div class="w-full flex justify-center pt-10">
+        <div class="flex flex-col w-3/4">
+            <UAlert
+                icon="i-heroicons-command-line"
+                color="primary"
+                variant="solid"
+                title="Nothing to review now!"
+                description="You completed review all the words before the deadline."
+            />
+        </div>
+    </div>
+</div>
+<USlideover v-model="isShowFullWord" prevent-close side="bottom">
+      <UCard class="flex flex-col flex-1" :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+              Slideover
+            </h3>
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isShowFullWord = false" />
+          </div>
+        </template>
+
+        <Placeholder class="h-full" />
+      </UCard>
+    </USlideover>
 </template>
