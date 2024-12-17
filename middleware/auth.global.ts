598de40db65b699d4464cba5a37173b24def994e
VietDung <vietdung.7301@gmail.com>
2024-12-17 23:58:13 +0900

    Fix infinite repeat loading page

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -24,6 +24,7 @@ export default defineNuxtRouteMiddleware((to) => {
     //     return navigateTo('/');
     // }
     if (Number.isNaN(user.value.id) && to.path !== '/login') {
+        console.log("Day la cookie", user.value)
         return navigateTo('/login');
     }
 })
