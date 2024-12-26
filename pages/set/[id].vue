3c943f8f1063fb56bb85599b19210460cf26e71c
Dodo-aiday <donguyen33m@gmail.com>
2024-12-26 08:31:07 +0000

    update word

--- pages/set/[id].vue
+++ pages/set/[id].vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import type { CardSet, Card } from '~/types/type';
+const toast = useToast()
+
+
 const { user_id } = storeToRefs(useAuthStore());
 const { id: setId } = useRoute().params
+
 
 const setList = await $fetch<CardSet[]>(`/api/card_set?user_id=${user_id.value}`, {
     method: "GET",
@@ -11,7 +15,39 @@ const cardList = await $fetch<Card[]>(`/api/card_set/${setId}`, {
     method: "GET",
 })
 
-const isEdit = false;
+const isEdit = ref(false);
+
+const state = reactive({
+	currentEditingCard: {} as Card
+})
+
+const handleClickEdit = (card:Card) => {
+    isEdit.value = true;
+    state.currentEditingCard = {...card}
+}
+
+const handleUpdateWord = () => {
+	$fetch(`/api/card/${state.currentEditingCard.id}`, {
+		method: "PUT",
+		body: {
+            ...state.currentEditingCard
+		}
+	}).finally( () => 
+		toast.add({title: "Update word success"})
+	)
+}
+
+const handleDeleteWord = () => {
+    console.log(`${state.currentEditingCard.id} Welcome to Delete!`);
+    $fetch(`/api/card/${state.currentEditingCard.id}`, {
+		method: "DELETE",
+		body: {
+            ...state.currentEditingCard
+		}
+	}).finally( () => 
+		toast.add({title: "Delete word success"})
+	)
+}
 
 const items = [
   [{
@@ -45,10 +81,37 @@ const items = [
                 </div>
                 <div class="italic">{{ card.example }}</div>
             </UCard>
-            <div class="w-24 mr-2 min-h-36 grid grid-col-2">
-                <UButton icon="mingcute:pencil-2-line" class="mb-2 ml-2" @click="isEdit = !isEdit">Edit</UButton>
-                <UButton icon="mingcute:delete-2-line" color="red" class="mt-2 ml-2">Delete</UButton>
+            <div class="w-24 mr-2 min-h-36 grid grid-col-2 justify-center">
+                <UButton icon="mingcute:pencil-2-line" class="mb-2 ml-2 p-2" variant="soft" @click="handleClickEdit(card)">Edit</UButton>
+                <UButton icon="mingcute:delete-2-line" color="red" variant="soft" class="ml-2 p-2" @click="handleDeleteWord">Delete</UButton>
             </div>
         </div>
     </div>
+    <UModal v-model="isEdit" :overlay="true">
+        <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+              Edit
+            </h3>
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isEdit = false" />
+          </div>
+        </template>
+        <div>
+            <UFormGroup label="Word" name="word">
+                <UInput v-model="state.currentEditingCard.word" class="mb-3"/>
+            </UFormGroup>
+            <UFormGroup label="Pronounciation" name="pronunciation">
+                <UInput v-model="state.currentEditingCard.pronunciation" class="mb-3"/>
+            </UFormGroup>
+            <UFormGroup label="Meaning" name="meaning">
+                <UInput v-model="state.currentEditingCard.meaning" class="mb-3"/>
+            </UFormGroup>
+            <UFormGroup label="Example" name="example">
+                <UInput v-model="state.currentEditingCard.example" class="mb-3"/>
+            </UFormGroup>
+            <UButton icon="mingcute:check-circle-fill" type="submit" @click="handleUpdateWord">Submit</UButton>
+        </div>
+      </UCard>
+    </UModal>
 </template>
