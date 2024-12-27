018fa313a3cb29f348e528b2d133d24a059df953
Hoàng Việt Dũng <vietdung.7301@gmail.com>
2024-12-27 01:15:12 +0000

    Update word

--- pages/set/[id].vue
+++ pages/set/[id].vue
@@ -49,14 +49,6 @@ const handleDeleteWord = () => {
 	)
 }
 
-const items = [
-  [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-    }
-  }]
-]
 </script>
 
 <template>
